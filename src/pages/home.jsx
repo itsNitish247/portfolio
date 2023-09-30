1ac1c3c07850a960573724e246e9d8c546f7316b
NITISH <meganitish@gmail.com>
2023-09-30 22:25:36 +0530

    added social icons

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,10 @@
 import { React, useEffect, useState } from "react";
 import "../styles/home.css";
+import GitHubIcon from '@mui/icons-material/GitHub';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 function Home() {
   const [title, setTitle] = useState("Web Developer");
@@ -21,8 +26,27 @@ function Home() {
       <h1 className="intro">
         Hi, I'm
         <span className="name"> Nitish Kumar</span>
-      </h1>
+      </h1> 
       <h2 className="profession">A {title}</h2>
+
+      
+      <div className="social_icons">
+        <a href="https://github.com/itsNitish247" target="_blank" rel="noopener noreferrer">
+        <GitHubIcon className="icon" />
+        </a> 
+        <a href="www.google.com" target="_blank" rel="noopener noreferrer">
+        <InstagramIcon className="icon" />
+        </a>
+         <a href="www.google.com" target="_blank" rel="noopener noreferrer">
+        <FacebookIcon className="icon" />
+        </a>
+         <a href="www.google.com" target="_blank" rel="noopener noreferrer">
+        <LinkedInIcon className="icon" />
+        </a> 
+        <a href="www.google.com" target="_blank" rel="noopener noreferrer">
+        <TwitterIcon className="icon" />
+        </a>
+      </div>
     </div>
   );
 }

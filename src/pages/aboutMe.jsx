20efaa3c30f5ab6267558a0ee63dcc15a6d4ab42
NITISH <meganitish@gmail.com>
2023-10-01 23:19:48 +0530

    added buttons and downloadble cv

--- src/pages/aboutMe.jsx
+++ src/pages/aboutMe.jsx
@@ -2,8 +2,19 @@ import React from 'react';
 import { motion } from 'framer-motion'; 
 import '../styles/aboutMe.css';
 import profilepic from '../assets/profilepic.jpeg';
+import resume from '../assets/resume.pdf'
 
 function AboutMe() {
+
+  const handleDownlaod =()=>{
+const anchor=document.createElement('a');
+anchor.href=resume;
+anchor.download='resume.pdf';
+anchor.click();
+  };
+
+
+
 
   const animationVariants = {
     hidden: { opacity: 0, y: 50 },
@@ -22,7 +33,7 @@ function AboutMe() {
       </div>
       <div className="aboutme_content">
         <h1 className='about'>About <span className='me'>Me</span></h1>
-        <h2 className='heading'>Hi , let me make a beautiful website for you</h2>
+        <h2 className='heading'>Hey , let me make a beautiful website for you</h2>
         <p className='para'>I am a dedicated and creative individual with a 
           passion for technology and a knack for problem-solving.
           As an enthusiastic learner, I enjoy exploring new domains
@@ -31,6 +42,9 @@ function AboutMe() {
           I am constantly seeking opportunities to
           apply my skills and make a positive impact. Whether it's coding, learning new languages, or pursuing personal projects, I thrive on continuous growth and innovation.
         </p>
+       <div className="buttons">
+        <button className='hireMe'>hire me</button>
+        <button className='cv' onClick={handleDownlaod}>Download CV</button></div>
       </div>
     </motion.div>
   );

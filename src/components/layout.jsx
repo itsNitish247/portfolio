d4bd6b72bfe13411a0f2e51ae00149f3395be078
NITISH <meganitish@gmail.com>
2023-10-02 16:15:32 +0530

    added experience page

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -3,6 +3,7 @@ import Navbar from './navbar'
 import Home from '../pages/home'
 import '../styles/layout.css'
 import AboutMe from '../pages/aboutMe'
+import Experience from '../pages/experience'
 
 
 function Layout() {
@@ -11,6 +12,7 @@ function Layout() {
       <div className='nav_placement'><Navbar /></div>
       <div className='home_placement'><Home /></div>
       <div className="aboutMe_placement"><AboutMe /></div>
+      <div className="experience_placement"><Experience /></div>
     </div>
   )
 } 
--- src/pages/experience.jsx
+++ src/pages/experience.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
+import '../styles/experience.css'
 
 function Experience() {
   return (
-    <div>
-      
+    <div className="main">
+    <h3 className="head">What i <span className='do'>do ?</span></h3>
+    <div className="container">
+      <ul>
+        <li>
+          <h3 className="heading">Applications engineer</h3>
+          <p>
+          developing springboot applications , helping clients
+          </p>
+          
+          <span className="date">2022 Dec-Present</span>
+          <span className="circle"></span>
+        </li>
+        </ul>
     </div>
-  )
+  </div>
+);
 }
+
+
+
 
 export default Experience

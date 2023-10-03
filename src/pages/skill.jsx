dc6e2b70e2ab9bddce0ee717f4690fe14d6ad468
Nitish <meganitish@gmail.com>
2023-10-03 16:55:41 +0530

    added skill page

--- src/pages/skill.jsx
+++ src/pages/skill.jsx
@@ -1,70 +1,94 @@
-  import React from 'react'
-  
+import React, { useState } from 'react';
+import { motion } from 'framer-motion'; // Import Framer Motion
+import vsCode from '../assets/svg/visual-studio-code.svg';
+import eclipse from '../assets/svg/eclipse.svg';
+import sql from '../assets/svg/mysql.svg';
+import firebase from '../assets/svg/firebase.svg';
+import expo from '../assets/svg/expo.svg';
+import intellij from '../assets/svg/intellij-idea.svg';
+import apache from '../assets/svg/apache.svg';
 
-  import { Container, Typography, Grid } from '@mui/material';
+import javascript from '../assets/svg/javascript.svg';
+import html from '../assets/svg/html.svg';
+import css from '../assets/svg/css.svg';
+import reactNative from '../assets/svg/react-native.svg';
+import react from '../assets/svg/react.svg';
+import java from '../assets/svg/java.svg';
+import '../styles/skill.css';
 
-  
-  function Skill() {
-    return (
-      <Container className="container">
+function Skill() {
+  const [showSkills, setShowSkills] = useState(true);
+
+  const toggleSkillsTools = () => {
+    setShowSkills(!showSkills);
+  };
+
+  return (
+    <div className="skill-container">
+      <h1>skill</h1>
+      <div className="toggle-switch">
+        <span className={`toggle-button ${showSkills ? 'active' : ''}`} onClick={toggleSkillsTools}>
+          Skills
+        </span>
+        <span className={`toggle-button ${showSkills ? '' : 'active'}`} onClick={toggleSkillsTools}>
+          Tools
+        </span>
+      </div>
+      <div className="container">
         <div className="section">
-          <Typography variant="h2">Tools</Typography>
-          <Grid container alignItems="center">
-            <Grid item>
-              <img src="path/to/vscode.svg" alt="VS Code" className="icon" />
-            </Grid>
-            <Grid item>
-              <span>VS Code</span>
-            </Grid>
-          </Grid>
-          <Grid container alignItems="center">
-            <Grid item>
-              <img src="path/to/eclipse.svg" alt="Eclipse" className="icon" />
-            </Grid>
-            <Grid item>
-              <span>Eclipse</span>
-            </Grid>
-          </Grid>
+          <h2>{showSkills ? 'Skills' : 'Tools'}</h2>
+          <div className="grid-container">
+            {showSkills ? ( 
+              <>
+                <motion.div className="grid-item" whileHover={{ scale: 1.2 }} initial={{ scale: 0 }} animate={{ scale: 1 }} transition={{ duration: 0.5 }}>
+                  <img src={react} alt="React" className="icon" />
+                </motion.div>
+                <motion.div className="grid-item" whileHover={{ scale: 1.2 }}  initial={{ scale: 0 }} animate={{ scale: 1 }} transition={{ duration: 0.5 }}>
+                  <img src={html} alt="HTML" className="icon" />
+                </motion.div>
+                <motion.div className="grid-item" whileHover={{ scale: 1.2 }} initial={{ scale: 0 }} animate={{ scale: 1 }} transition={{ duration: 0.5 }}>
+                  <img src={css} alt="CSS" className="icon" />
+                </motion.div>
+                <motion.div className="grid-item" whileHover={{ scale: 1.2 }} initial={{ scale: 0 }} animate={{ scale: 1 }} transition={{ duration: 0.5 }}>
+                  <img src={javascript} alt="JavaScript" className="icon" />
+                </motion.div>
+                <motion.div className="grid-item" whileHover={{ scale: 1.2 }} initial={{ scale: 0 }} animate={{ scale: 1 }} transition={{ duration: 0.5 }}>
+                  <img src={reactNative} alt="react-native" className="icon" />
+                </motion.div>
+                <motion.div className="grid-item" whileHover={{ scale: 1.2 }}  initial={{ scale: 0 }} animate={{ scale: 1 }} transition={{ duration: 0.5 }}>
+                  <img src={java} alt="Java" className="icon" />
+                </motion.div>
+              </>
+            ) : (
+              <>
+                <motion.div className="grid-item" whileHover={{ scale: 1.2 }} initial={{ scale: 0 }} animate={{ scale: 1 }} transition={{ duration: 0.5 }}>
+                  <img src={vsCode} alt="VS Code" className="icon" />
+                </motion.div>
+                <motion.div className="grid-item" whileHover={{ scale: 1.2 }} initial={{ scale: 0 }} animate={{ scale: 1 }} transition={{ duration: 0.5 }}>
+                  <img src={firebase} alt="Firebase" className="icon" />
+                </motion.div>
+                <motion.div className="grid-item" whileHover={{ scale: 1.2 }} initial={{ scale: 0 }} animate={{ scale: 1 }} transition={{ duration: 0.5 }}>
+                  <img src={sql} alt="SQL" className="icon" />
+                </motion.div>
+                <motion.div className="grid-item" whileHover={{ scale: 1.2 }} initial={{ scale: 0 }} animate={{ scale: 1 }} transition={{ duration: 0.5 }}>
+                  <img src={expo} alt="Expo" className="icon" />
+                </motion.div>
+                <motion.div className="grid-item" whileHover={{ scale: 1.2 }} initial={{ scale: 0 }} animate={{ scale: 1 }} transition={{ duration: 0.5 }}>
+                  <img src={eclipse} alt="Eclipse" className="icon" />
+                </motion.div>
+                <motion.div className="grid-item" whileHover={{ scale: 1.2 }} initial={{ scale: 0 }} animate={{ scale: 1 }} transition={{ duration: 0.5 }}>
+                  <img src={intellij} alt="IntelliJ Idea" className="icon" />
+                </motion.div>
+                <motion.div className="grid-item" whileHover={{ scale: 1.2 }} initial={{ scale: 0 }} animate={{ scale: 1 }} transition={{ duration: 0.5 }}>
+                  <img src={apache} alt="Apache" className="icon" />
+                </motion.div>
+              </>
+            )}
+          </div>
         </div>
-        <div className="section">
-          <Typography variant="h2">Skills</Typography>
-          <Grid container alignItems="center">
-            <Grid item>
-              <img src="path/to/react.svg" alt="React" className="icon" />
-            </Grid>
-            <Grid item>
-              <span>React</span>
-            </Grid>
-          </Grid>
-          <Grid container alignItems="center">
-            <Grid item>
-              <img src="path/to/html.svg" alt="HTML" className="icon" />
-            </Grid>
-            <Grid item>
-              <span>HTML</span>
-            </Grid>
-          </Grid>
-          <Grid container alignItems="center">
-            <Grid item>
-              <img src="path/to/css.svg" alt="CSS" className="icon" />
-            </Grid>
-            <Grid item>
-              <span>CSS</span>
-            </Grid>
-          </Grid>
-          <Grid container alignItems="center">
-            <Grid item>
-              <img src="path/to/javascript.svg" alt="JavaScript" className="icon" />
-            </Grid>
-            <Grid item>
-              <span>JavaScript</span>
-            </Grid>
-          </Grid>
-        </div>
-      </Container>
-    );
-  }
-  
-  export default Skill;
-  
-  
+      </div>
+    </div>
+  );
+}
+
+export default Skill;
